349e51765b047e9aefc08011b989816adc0585e0
Daniil Beloglazov <zheltogsan@gmail.com>
2023-04-22 21:59:00 +0500

    Default Ktor stuff by IDEA

---
+++ preasy/src/main/kotlin/seven/belog/Application.kt
@@ -0,0 +1,15 @@
+package seven.belog
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import seven.belog.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+            .start(wait = true)
+}
+
+fun Application.module() {
+    configureRouting()
+}
---
+++ preasy/src/test/kotlin/seven/belog/ApplicationTest.kt
@@ -0,0 +1,21 @@
+package seven.belog
+
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.server.testing.*
+import kotlin.test.*
+import io.ktor.http.*
+import seven.belog.plugins.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+    }
+}
